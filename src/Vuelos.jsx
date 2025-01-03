e00461fdf58b648b5b50f09968f35bd7abf75022
MikeArJay <michael.jam13@gmail.com>
2025-01-03 18:08:25 +0100

    correccion carpeta formularioVuelos

--- src/Vuelos.jsx
+++ src/Vuelos.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import importedVuelos from "./vuelos.json"
 import Vuelo from "./Vuelo.jsx"
-import FormularioVuelo from "./assets/FormularioVuelo.jsx";
+import FormularioVuelo from "./FormularioVuelo.jsx";
 
 function Vuelos() {
     const [data, setData] = useState(importedVuelos);
@@ -9,16 +9,8 @@ function Vuelos() {
 
     const addFlight = (flight, destination) => {
 
-        /*TODO setData With info form child
-        {
-                "date": "2023-12-12",
-                "time": "10:00",
-                "number": "VO123",
-                "seats": 150
-            }
-            */
         let newData = JSON.parse(JSON.stringify(data))
-        // check
+ 
         newData.forEach((item)=>{
             if (item.destination === destination){
                 item.flights.push(flight)
